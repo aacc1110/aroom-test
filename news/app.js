d85660a8c8de1754ea9a180e2b7b611ca1b1889e
aacc1110 <tadrow@daum.net>
2018-11-01 08:49:30 +0900

    aroom

--- news/app.js
+++ news/app.js
@@ -1,9 +1,28 @@
-var async = require('async');
+
 const apiKey = 'f3718a4d1a5c4d059d5b955713de5a5f';
 const main = document.querySelector('main');
-window.addEventListener('load', e => {
+const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource = 'the-washington-post';
+
+
+window.addEventListener('load', async e => {
     updateNews();
+    await updateSources();
+    sourceSelector.value = defaultSource ;
+
+    sourceSelector.addEventListener('change', e => {
+        updateNews(e.target.value);
+    });
+
 });
+
+async function updateSources(){
+    const res = await fetch(`https://newsapi.org/v1/sources`);
+    const json = await res.json();
+
+    sourceSelector.innerHTML = json.sourcemap(src => `<option value="${src.id}">${src.name}</option>`).join('\n');
+
+}
 
 async function updateNews() {
     const res = await fetch(`https://newsapi.org/v2/everything? q = bitcoin & from = 2018-09-30 & sortBy = publishedAt & apiKey = ${apiKey}`);
