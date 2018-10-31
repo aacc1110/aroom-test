cc5b7f172d7b531015c6ab828f8a6df6f7646753
aacc1110 <tadrow@daum.net>
2018-10-31 19:38:03 +0900

    aroom

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var http = require('http');
+var https = require('https');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+
+var options = {  
+  key: fs.readFileSync('keys/private.pem'),
+  cert: fs.readFileSync('keys/public.pem'),
+  agent: false
+};
  
-var app = http.createServer(function(request,response){
+var app = https.createServer(options, function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
