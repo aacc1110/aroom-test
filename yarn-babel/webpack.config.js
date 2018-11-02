516adf1a21e197aff32fb482d68f62883a4266dd
aacc1110 <tadrow@daum.net>
2018-11-02 23:48:31 +0900

    aroom

--- yarn-babel/webpack.config.js
+++ yarn-babel/webpack.config.js
@@ -1,25 +1,27 @@
-'use strict'
+'use strict';
+
 const path = require('path');
 
-module.exports = {
+module.exports = ({production = false, ssr = false, lite = false} = {}) => {
+//  process.env.NODE_ENV = production ? 'production' : 'development';
+  const webpackConfig = {
     entry: {
-        main: ['./src/main.js']
-    },
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: '[name].js'
+      main: ['./src/main.js'],
+      vendor: (lite ? [] : ['./src/stdlib.js']).concat(['react', 'react-dom'])
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            include:: path.resolve(__dirname, './src'),
-            loaders: 'babel-loader'
-        }]
+      loaders: [{
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, './src'),
+        loaders: 'babel-loader'
+      }]
     },
     plugins: [],
     devServer: {
-        contentBase: '.yarn-babel',
-        host: 'localhost',
-        prot: 8000
+      contentBase: './yarn-babel',
+      inline: true,
+      host: 'localhost',
+      port: 8000
     }
+  };
 };
