d6d00ab97d3d8f9e6bdd518b7994c640f6bcdb33
aacc1110 <tadrow@daum.net>
2018-11-07 16:08:13 +0900

    aroom

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express1' });
+  res.render('index.html', { title: 'Express1', name: 'HSC', page: 'Home page' });
 });
 
 module.exports = router;
